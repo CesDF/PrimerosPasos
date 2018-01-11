3d1e239beff74f3bb00afbd4bf9cf43d7aeeca77
CesarDiaz <enzo.cdf@hotmail.com>
2018-01-11 11:15:32 +0000

    Commit inicial

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		byte edad=35;
+		//la variable se puede declarar en la misma línea en la que se inicia salvo en los casos en los que no sabes su valor
+		edad=45;
+		
+		System.out.println(edad);/*La siguiente instrucción imprime en la consola
+		* el valor de la variable edad
+		*/
+		
+		edad=30;
+		
+		System.out.println(edad);
+		
+	}
+
+}
